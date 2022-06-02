947ba0d08a63e6c52d98b5e0ad74e349e77081ac
Tasha Davis <tdavis0831@gmail.com>
2022-06-02 12:49:23 -0400

    initial commit

---
+++ add_two_integers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        
+        total= num1+num2
+        
+        return total
+    
+    #take the 2 numbers and add them- you can also just add them without storing them as a variable
